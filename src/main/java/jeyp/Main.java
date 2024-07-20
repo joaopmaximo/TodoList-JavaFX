02806b42a875b30f656e1667521bdfe07f4fca84
joaopmaximo <jp.maximoo@gmail.com>
2024-07-20 04:59:49 -0300

    first commit

---
+++ src/main/java/jeyp/Main.java
@@ -0,0 +1,35 @@
+package jeyp;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/jeyp/main.fxml"));
+            Parent root = loader.load(); // root node, required by scene
+            Scene scene = new Scene(root); // scene, required by stage
+            String css = getClass().getResource("/css/styles.css").toExternalForm();
+            scene.getStylesheets().add(css);
+            Image icon = new Image("/img/lista.png");
+    
+            stage.setTitle("JeypToDo");
+            stage.getIcons().add(icon);
+            stage.setScene(scene);
+            stage.setResizable(false);
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
---
+++ src/main/java/jeyp/MainController.java
@@ -0,0 +1,12 @@
+package jeyp;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.Pane;
+
+public class MainController { 
+
+    @FXML
+    private Pane mainPane;
+
+    
+}
