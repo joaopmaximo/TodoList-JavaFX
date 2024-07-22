75c644d2d2cc7c07ecd93cddcfd9aa4794c927ef
joaopmaximo <jp.maximoo@gmail.com>
2024-07-22 01:44:51 -0300

    working, saving in txt file

--- src/main/java/todoList/controller/MainController.java
+++ src/main/java/todoList/controller/MainController.java
@@ -1,5 +1,8 @@
 package todoList.controller;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 
 import javafx.fxml.FXML;
@@ -21,6 +24,29 @@ public class MainController {
     @FXML
     private TextField newTaskField;
 
+    public void getTasks() throws FileNotFoundException {
+        FileReader fileReader = new FileReader("tasks.txt");
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+        @SuppressWarnings("unused")
+        String line;
+
+        try {
+            while ((line = bufferedReader.readLine()) != null) {
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/task.fxml"));
+                HBox taskItem = loader.load();
+                TaskController taskController = loader.getController();
+                Task task = new Task(line);
+                taskController.setData(task);
+                taskList.getChildren().add(taskItem);
+            }
+            bufferedReader.close();
+            fileReader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     public void addTask() {
         try {
             Task task = new Task(newTaskField.getText());
@@ -28,10 +54,10 @@ public class MainController {
             HBox taskItem = loader.load();
             TaskController taskController = loader.getController();
             taskController.setData(task);
+            taskController.saveTask(task);
             taskList.getChildren().add(taskItem);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-
 }
--- src/main/java/todoList/controller/TaskController.java
+++ src/main/java/todoList/controller/TaskController.java
@@ -1,5 +1,8 @@
 package todoList.controller;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import todoList.model.Task;
@@ -11,6 +14,12 @@ public class TaskController {
 
     public void setData(Task task) {
         text.setText(task.getText());
+    }
+
+    public void saveTask(Task task) throws IOException {
+        FileWriter fileWriter = new FileWriter("tasks.txt", true);
+        fileWriter.write(task.getText() + "\n");
+        fileWriter.close();
     }
 
 }
